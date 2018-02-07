06ae16e6070345f99c380c4285f4a155d8e9c564
Leonilo P Salve jr <jrsalve@Leonilos-MacBook-Pro.local>
2018-02-07 11:39:56 +0800

    Change page title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('title')
+{{ $page->title }}
+@endsection
 
 @section('content')
 <div class="container">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel2') }}</title>
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+    <title> @yield('title') </title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
